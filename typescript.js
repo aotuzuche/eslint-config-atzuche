9b5b49d4ffea1813d01008ffb04ff237cfbe75c8
Eason chiu <76670758@qq.com>
2020-07-19 18:28:46 +0800

    箭头函数允许使用this

--- typescript.js
+++ typescript.js
@@ -43,5 +43,6 @@ module.exports = {
     '@typescript-eslint/explicit-member-accessibility': ['error', { accessibility: 'no-public' }],
     '@typescript-eslint/no-require-imports': 'off',
     '@typescript-eslint/no-invalid-this': 'off',
+    'react/no-this-in-sfc': 'off',
   },
 }
