b57855873f8655079e6100512f7a7cebf4d4ba64
Eason chiu <76670758@qq.com>
2021-09-16 15:23:38 +0800

    feat: upgrade version

--- react.js
+++ react.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: ['react-hooks'],
-  extends: ['alloy', 'alloy/react', 'prettier/react', './base.js'],
+  extends: ['alloy', 'alloy/react', 'prettier', './base.js'],
   globals: {
     // 这里填入你的项目需要的全局变量
     // 这里值为 false 表示这个全局变量不允许被重新赋值，比如：
